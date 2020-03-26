d85f0e35a17e7c7121f26d3b8591f55f0bceb9c1
reganto <rreganto@gmail.com>
2020-03-26 22:05:20 +0430

    app read configs from config.py

--- trackstar/__init__.py
+++ trackstar/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from config import config
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config.get(config_name))
+    config[config_name].init_app(app)
 
     from .users import users as users_blueprint
     app.register_blueprint(users_blueprint, url_prefix="/users")
