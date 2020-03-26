12572267d5acc5505e4caff0b94c242dbd03e088
reganto <rreganto@gmail.com>
2020-03-26 23:29:27 +0430

    add test command to trackstar

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ from trackstar.models import User
 app = create_app("default")
 migrate = Migrate(app, db)
 
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
