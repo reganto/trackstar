a73acb6632598725e9778934541842c002a130b3
reganto <rreganto@gmail.com>
2020-03-26 22:33:14 +0430

    trackstar models

---
+++ trackstar/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True, index=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
