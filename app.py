fc3065aac2a02b11544ced8e8c79e2ce9d3d7d6e
reganto <rreganto@gmail.com>
2020-03-27 19:58:44 +0430

    add coverage --coverage/--no-coverage command line option to flask test command

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import click 
+
 from flask_migrate import Migrate
 from trackstar import create_app, db
 from trackstar.models import User
@@ -5,12 +9,32 @@ from trackstar.models import User
 app = create_app("default")
 migrate = Migrate(app, db)
 
+COV = None
+if os.environ.get("FLASK_COVERAGE"):
+    import coverage
+    COV = coverage.coverage(branch=True, include="trackstar/*")
+    COV.start
+
 
 @app.cli.command()
-def test():
-    """Run the unit tests."""
-
+@click.option("--coverage/--no-coverage", default=False,
+              help="Run tests under code coverage.")
+def test(coverage):
+    """Run the unit tests"""
+    if coverage and not os.environ.get("FLASK_COVERAGE"):
+        os.environ["FLASK_COVERAGE"] = "1"
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print("Coverage Summary:")
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, "tmp/coverage")
+        COV.html_report(directory=covdir)
+        print("HTML version: file://%s/index.html" % covdir)
+        COV.erase()
 
