23b3907b981da941d1c371b00f220e8df483ab68
reganto <rreganto@gmail.com>
2020-03-28 00:08:38 +0430

    add projects model

--- trackstar/models.py
+++ trackstar/models.py
@@ -8,3 +8,15 @@ class User(db.Model):
     username = db.Column(db.String(128), unique=True, index=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
 
+
+class Project(db.Model):
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    create_time = db.Column(db.DateTime, default=None)
+    create_user_id = db.Column(db.Integer, default=None)
+    update_time = db.Column(db.DateTime, default=None)
+    update_user_id = db.Column(db.Integer, default=None)
+
